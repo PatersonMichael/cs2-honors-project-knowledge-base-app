f8dc4767f3908452317dcfcdfa8ff4516c1486e4
PatersonMichael <michael.paterson7@gmail.com>
2023-09-27 00:33:48 -0700

    Added SQLServer folder, CreateDatabase.sql

---
+++ Database/SQLServer/CreateDatabase.sql
@@ -0,0 +1,38 @@
+-- Create Database 
+USE master;
+GO
+
+CREATE DATABASE KnowledgeBase;
+GO
+
+-- Create Schema for KnowledgeBase
+
+USE KnowledgeBase;
+GO 
+
+CREATE SCHEMA KnowledgeBase
+    AUTHORIZATION dbo;
+GO
+
+-- Create filegroups
+
+
+-------------------!!!!!! SET BOTH FILES TO SAME DRIVE IF HOST SYSTEM ONLY HAS ONE DRIVE !!!!!!-----------------------------
+ALTER DATABASE KnowledgeBase
+    ADD FILEGROUP KnowledgeBase_dat;
+ALTER DATABASE KnowledgeBase
+    ADD FILE (
+        NAME = KnowledgeBase_dat,
+        FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\knowledgebasedat.mdf'
+    )
+    TO FILEGROUP KnowledgeBase_dat;
+
+ALTER DATABASE KnowledgeBase
+    ADD LOG FILE (
+        NAME = KnowledgeBase_log,
+        -- Pick one filename, if system only has one drive, choose c drive, otherwise choose D or define the drive you will use.
+        -- FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\KnowledgeBaselog.ldf'
+        -- FILENAME = 'D:\SQL\SQLEXPRESS\KnowlegeBase\LOGS\KnowledgeBaselog.ldf'
+    );
+
+GO
