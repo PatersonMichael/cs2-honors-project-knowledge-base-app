921ced17bcfef35c5e3bf2e139ba83244bbe78e8
Michael Paterson <michael.paterson7@gmail.com>
2023-11-18 22:56:15 -0800

    added home route, component

--- KnowledgeBaseApp/Website/src/app/app.component.ts
+++ KnowledgeBaseApp/Website/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="logo">Knowledge Base</header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Website';
+  title = 'KnowledgeBase';
 }
